d1f4fbdeef9d959c182220b2b887faf9c457abcf
Andrea <siizx@live.it>
2024-06-01 12:45:17 +0200

    finito tutti gli esercizi di lagorio posix 2. svolti in maniera 'pigra' per mancanza di tempo, ma funzionanti e corretti al 100%. comincio microbash.

--- lagorio/posix2_esercizi/es5/main.c
+++ lagorio/posix2_esercizi/es5/main.c
@@ -2,6 +2,13 @@
 // usate precedentemente, la system-call pipe(2).
 // ANCHE QUEST ULTIMO ESERCIZIO verra' implementato in maniera 'pigra' per mancanza di tempo.
 
+//  _____     _   _        _ 
+// |  ___|_ _| |_| |_ ___ | |
+// | |_ / _` | __| __/ _ \| |
+// |  _| (_| | |_| || (_) |_|
+// |_|  \__,_|\__|\__\___/(_)
+                          
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/wait.h>
@@ -29,25 +36,25 @@ void fail_errno(char* str){
 
 void execPs(int pipefd[], const char ps[], const char psArg[],const char psCommandPath[]){
 
-	close(pipefd[0]); // chiudo l'fd di lettura
 
 	pid_t pid = fork(); // FORK()
 	if(pid == -1) fail_errno("fork() failed");
 	// fork succeeded: procedo con la gestione dei file descriptors.
 	if(!pid){ // FIGLIO
-		if(dup2(pipefd[1], STDIN_FILENO) == -1) fail_errno("dup2() ps failed");
+		close(pipefd[0]); // chiudo l'fd di lettura
+		if(dup2(pipefd[1], STDOUT_FILENO) == -1) fail_errno("dup2() ps failed");
 		if(execl(psCommandPath, ps, psArg, NULL) == -1) fail_errno("execl(ps) failed");
 	}
 }
 
 void execGrep(int pipefd[], const char grep[], const char grepCommandPath[], const char bash[]){
 
-	close(pipefd[1]); // chiudo l'fd di scrittura
 	pid_t pid = fork(); // FORK()
 	if(pid == -1) fail_errno("fork() failed");
 	// fork succeeded: procedo con la gestione dei file descriptors.
 	if(!pid){ // FIGLIO
-		if(dup2(STDOUT_FILENO, pipefd[0]) == -1) fail_errno("dup2() grep failed");
+		close(pipefd[1]); // chiudo l'fd di scrittura
+		if(dup2(pipefd[0], STDIN_FILENO) == -1) fail_errno("dup2() grep failed");
 		if(execl(grepCommandPath, grep, bash, NULL) == -1) fail_errno("grep() failed");
 	}
 }
@@ -70,14 +77,12 @@ int main(){
 	
 	execPs(pipefd, ps, psArg, psCommandPath);
 	execGrep(pipefd, grep, grepCommandPath, bash);
-	wait(NULL);	
-	wait(NULL);
-
+	// chiudo i fds PRIMA di usare le wait. senno' i processi figli non terminano.
 	close(pipefd[0]);
 	close(pipefd[1]);
-	if(!pid){ // FIGLIO
-				
-	}
+
+	wait(NULL);	
+	wait(NULL);
 
 	if(dup2(stdoutBackup, STDOUT_FILENO) == -1) fail_errno("dup2 restore stdout failed");
 	if(dup2(stdinBackup, STDIN_FILENO) == -1) fail_errno("dup2 restore stdin failed");
