3922328f109b113fdcb7f394f31661afb878e3d0
Andrea <siizx@live.it>
2024-06-02 18:59:53 +0200

    microbash a malapena iniziato. Volendo lo potrei anche ri-iniziare da capo, tato ho scritto 10 righe se va bene. Esame di SETI posticipato perche ci vuole troppo tempo, senza contare che poi ci sarebbe anche l'orale. tutto metterebbe i bastoni tra le ruote di ASD e Calculus. meglio rimandare. RECAP: PINGPONG finito. dare un occhiata generale e un'occhiata attenta al blocking(?). MICROBASH e INCAPACHE da fare.

---
+++ lagorio/tests/assert/assert.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+
+
+
+int main(){
+
+	int arg = 0;
+	assert(arg); // se arg == 0, l'asserzione fallisce e il programma viene arrestato. unmessaggio viene stampato in stderr
+	return 0;
+}
